b0ceca728e395b8649d343dfa7df58becac01077
ocni-dtu <ocni@dtu.dk>
2018-03-05 22:18:56 +0100

    update the sphinx docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -32,9 +32,10 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
