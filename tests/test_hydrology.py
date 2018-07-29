5e1dbf166b076477fae096a94440e4202a30a8e0
ocni-dtu <ocni@dtu.dk>
2018-07-29 14:11:43 +0200

    added test to mesh_to_cells

--- tests/test_hydrology.py
+++ tests/test_hydrology.py
@@ -5,9 +5,22 @@ __license__ = "MIT"
 # Imports
 
 # Module imports
-
+import cmf
 
 # Livestock imports
+from livestock import hydrology
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # Livestock Test
+
+
+def test_mesh_to_cells(obj_file_paths):
+    project = cmf.project()
+
+    hydrology.mesh_to_cells(project, obj_file_paths, False)
+
+    assert project
+    assert project.cells
+
+
+
